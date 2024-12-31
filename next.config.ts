9fa67a05aecce037ccd76cacc8286a567424ca33
hrysdev <hrysdev@gmail.com>
2025-01-01 03:36:17 +0900

    feat: 静的サイトとしてデプロイ
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
